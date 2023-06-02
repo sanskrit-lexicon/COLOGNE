d535abf7843cabfca2e3b8bb29d53308f1e67cfc
funderburkjim <funderburkjim@users.noreply.github.com>
2023-06-02 14:20:59 -0400

    Update download2.sh
    improve handling of 'year'

--- localinstall/mac/download2.sh
+++ localinstall/mac/download2.sh
@@ -1,18 +1,36 @@
 dictlo=$1
 #dictup=`echo $dictlo | tr 'a-z' 'A-Z'`
 dictup=`echo $dictlo | tr '[:lower:]' '[:upper:]'`
-echo "creating directory $dictlo"
-mkdir $dictlo
-cd $dictlo
+if [ -f "$dictup" ]; then
+    echo "$dictup directory exists"
+else 
+    echo "creating directory $dictlo"
+    mkdir $dictlo
+fi
+# the download url has a 'year' parameter: e.g. MWScan/2020
+# the 'year for most of these is 2020 (as of June 2, 2023).
+# but a few use another year
 year=2020   # for lrv, this must be 2022
+if [ "$dictlo" == "lrv" ]; then
+        year=2022
+    fi
+echo "Using year = $year";
+
+cd $dictlo
+
 zipfile=${dictlo}web1.zip
+if [ -f "$zipfile" ]; then
+    echo "removing previous zipfile ($zipfile)"
+    rm $zipfile
+fi
+
 url="https://www.sanskrit-lexicon.uni-koeln.de/scans/${dictup}Scan/$year/downloads/$zipfile"
 echo "downloading $url ..."
 curl $url -o $zipfile
 echo "unzipping $zipfile"
-unzip -q $zipfile   #-q = quiet
+unzip -qu $zipfile   #-q = quiet, -u only changed or new
 echo "$dictlo should be installed now"
 pwd
 ls
-echo "DONE with download1.sh $dictlo"
+echo "DONE with download2.sh $dictlo"
 

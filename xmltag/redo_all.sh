6b6d0b02bb67220fa1f75c8c18b05a605ae5a88c
funderburkjim <funderburkjim@gmail.com>
2024-02-02 21:14:24 -0500

    Update all_xmltags (to see which use 'pe' tag) in xxx.txt. #366

--- xmltag/redo_all.sh
+++ xmltag/redo_all.sh
@@ -1,7 +1,9 @@
 for dictlo in  acc ae ap90 ben   bhs bop bor bur cae \
  ccs gra gst ieg inm  krm mci md mw mw72 \
  mwe pe pgn pui    pw pwg sch shs skd \
- snp stc vcp vei wil  yat lan armh
+ snp stc vcp vei wil  yat lan armh \
+ pwkvn lrv abch acph acsj
+	      
 do
  sh redo_one.sh ${dictlo}
  #dictup="${dictlo^^}"

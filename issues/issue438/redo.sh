285a53c48da21cdc69b18442119a6fce05516007
drdhaval2785 <drdhaval2785@gmail.com>
2025-03-17 10:42:06 +0530

    abf, diff 84

--- issues/issue438/redo.sh
+++ issues/issue438/redo.sh
@@ -8,6 +8,6 @@ python reversal.py temp_pwg_2.xml temp_reverted_pwg_0.xml
 diff temp_pwg_0.xml temp_reverted_pwg_0.xml > log.diff
 cat log.diff | wc -l
 cp temp_reverted_pwg_0.xml ../../../useless/temp_pwg.xml
-cd ../../../useless
-git diff --word-diff
-cd ../COLOGNE/issues/issue438
+#cd ../../../useless
+#git diff --word-diff
+#cd ../COLOGNE/issues/issue438

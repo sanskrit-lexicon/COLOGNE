de2fd65b0498541a826751170abc1cb3d27eb26d
drdhaval2785 <drdhaval2785@gmail.com>
2025-03-19 20:25:56 +0530

    medini handled in expand_ls5

---
+++ issues/issue438/expand_ls5.py
@@ -0,0 +1,25 @@
+import sys
+import re
+
+def preprocess_ls_tags(content):
+    """Removes <is> tags inside <ls> tags."""
+    return re.sub(r'<ls>(MED\. )<is>(.*?)</is>', r'<ls>\1\2', content)
+
+def medini(ls_content):
+    """Handles special ls tags transformation for MED. references."""
+    return re.sub(r'<ls>(MED\.)\s+(\S+)\.?(\s*\d+\.?)*</ls>',
+                  r'<ls n="\1" id="\2\3">\1 \2\3</ls>',
+                  ls_content)
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    with open(input_file, "r", encoding="utf-8") as infile:
+        content = infile.read()
+    
+    content = preprocess_ls_tags(content)
+    transformed_content = medini(content)
+    
+    with open(output_file, "w", encoding="utf-8") as outfile:
+        outfile.write(transformed_content)

d818417cdb07c2c9157b6702efa77e20baab5352
drdhaval2785 <drdhaval2785@gmail.com>
2025-03-19 20:30:50 +0530

    redo update

--- issues/issue438/redo.sh
+++ issues/issue438/redo.sh
@@ -7,7 +7,7 @@ python expand_ls2.py temp_pwg_1.xml book_names1.txt temp_pwg_2.xml
 python reversal.py temp_pwg_2.xml temp_reverted_pwg_0.xml
 diff temp_pwg_0.xml temp_reverted_pwg_0.xml > log.diff
 cat log.diff | wc -l
-#cp temp_reverted_pwg_0.xml ../../../useless/temp_pwg.xml
-#cd ../../../useless
-#git diff --word-diff
-#cd ../COLOGNE/issues/issue438
+python expand_ls3.py temp_pwg_2.xml temp_pwg_3.xml
+python expand_ls4.py temp_pwg_3.xml book_names3.txt temp_pwg_4.xml
+python expand_ls5.py temp_pwg_4.xml temp_pwg_5.xml
+
